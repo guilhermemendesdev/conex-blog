ba1b7f3cb128234817e473169c588a6ded604101
Guilherme <guilhermemendesdev@gmail.com>
2024-07-09 05:48:41 -0300

    feat: add authors model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
 import { DatabaseModule } from './database/database.module'
 import { GraphQLModule } from '@nestjs/graphql'
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import path from 'path'
+import { AppResolver } from './app.resolver'
+import { AuthorsModule } from './authors/authors.module';
 
 @Module({
   imports: [
@@ -12,9 +14,10 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
     DatabaseModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
     }),
+    AuthorsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
