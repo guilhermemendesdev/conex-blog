554c90b901caaae049376f092159fd2ea8845d50
Guilherme <guilhermemendesdev@gmail.com>
2024-07-09 06:22:08 -0300

    feat: add authors repositories

---
+++ src/authors/interfaces/author.repository.ts
@@ -0,0 +1,27 @@
+import { Author } from '@prisma/client'
+import { ICreateAuthor } from './create-author'
+
+export interface ISearchParams {
+  page?: number
+  perPage?: number
+  filter?: string
+  sort?: string
+  sortDir?: 'asc' | 'desc'
+}
+export interface ISearchResult {
+  items: Author[]
+  currentPage: number
+  perPage: number
+  lastPage: number
+  total: number
+}
+
+export interface IAuthorRepository {
+  create(data: ICreateAuthor): Promise<Author>
+  update(data: Author): Promise<Author>
+  delete(id: string): Promise<Author>
+  findById(id: string): Promise<Author>
+  findByEmail(email: string): Promise<Author>
+  search(params: ISearchParams): Promise<ISearchResult>
+  get(id: string): Promise<any>
+}
